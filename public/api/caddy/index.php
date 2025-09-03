5564f6cc9d90ee72c21389f6599274542e16fb05
WEBDERNargor <ditsarutsukkong@gmail.com>
2025-09-03 09:02:46 +0700

    update path

---
+++ public/api/caddy/index.php
@@ -0,0 +1,3 @@
+<?php
+// Public shim to route /api/caddy/* when DocumentRoot is the public directory
+require_once __DIR__ . '/../../../api/caddy/index.php';
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+// Front controller for public DocumentRoot that boots the app from project root
+require_once __DIR__ . '/../index.php';
